faa8713bf6bc56dd88f68c7c77bfca0aa1921d18
Toronyi Zsombor <toronyi.zsombor.dev@gmail.com>
2026-03-07 11:52:41 +0100

    Add Necessary Enums

---
+++ Core/Enums/AppType.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Enums;
+
+public enum AppType
+{
+    WebApi,
+    Blazor,
+    Mvc
+}
---
+++ Core/Enums/DeploymentStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Enums;
+
+public enum DeploymentStatus
+{
+    Pending,
+    InProgress,
+    Succeeded,
+    Failed
+}
