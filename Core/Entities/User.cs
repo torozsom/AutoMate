33edb2edd2672b61b8aec87b141c3efa51997c7e
Toronyi Zsombor <toronyi.zsombor.dev@gmail.com>
2026-03-07 11:32:17 +0100

    Add User to Entities

---
+++ Core/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+
+    public required string GitHubUsername { get; set; }
+
+    public required string Email { get; set; }
+
+    public string? GitHubAccessToken { get; set; }
+
+    public ICollection<Project> Projects { get; set; }
+}
