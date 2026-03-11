17c2fb8e63ab7a70211b5a8f3e85bda998ec7b41
Toronyi Zsombor <toronyi.zsombor.dev@gmail.com>
2026-03-11 18:52:50 +0100

    Refactor Card Styling and Centralize Route Configuration

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,8 +1,6 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Services.Data;
-using Web.Components;
 
 
 // This is the main entry point for the ASP.NET Core web application.
@@ -73,29 +71,8 @@ app.UseAuthorization();
 // Use antiforgery middleware to protect against cross-site request forgery (CSRF) attacks.
 app.UseAntiforgery();
 
-// Map static assets (like CSS, JavaScript, and images) to be served directly from the wwwroot folder.
-app.MapStaticAssets();
-
-// Map Razor components to the root URL ("/") and configure them to use interactive server render mode,
-// which allows for dynamic, server-rendered components that can update in real-time without requiring a full page refresh.
-app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
-
-// Map a GET endpoint for "/login" that initiates the authentication challenge
-// using the GitHub authentication scheme, redirecting the user to the GitHub
-// login page and then back to the root URL ("/") upon successful authentication.
-app.MapGet("/login", () =>
-    Results.Challenge(
-        new AuthenticationProperties { RedirectUri = "/" },
-        ["GitHub"])
-);
-
-// Map a POST endpoint for "/logout" that signs the user out of the application
-// by clearing the authentication cookie and then redirects them back to the root.
-app.MapPost("/logout", async (HttpContext context) =>
-{
-    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-    return Results.Redirect("/");
-});
+// Configure endpoints
+Web.Routes.Configure(app);
 
 // Run the application, starting the web server and listening for incoming HTTP requests.
 app.Run();
