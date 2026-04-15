9bdc1e86316e62abb72927f5e58cf9055101ff9d
Toronyi Zsombor <toronyi.zsombor.dev@gmail.com>
2026-04-16 00:10:25 +0200

    Refactor Entities for Docker Deployment and Update DB

--- Core/Enums/DeploymentStatus.cs
+++ Core/Enums/DeploymentStatus.cs
@@ -1,28 +1,45 @@
 ﻿namespace Core.Enums;
 
 /// <summary>
-///     Defines the possible statuses of a deployment,
-///     including Pending, InProgress, Succeeded, and Failed.
+///     Defines the possible statuses of a deploymen.
 /// </summary>
 public enum DeploymentStatus
 {
     /// <summary>
-    ///     The deployment is pending and has not yet started.
+    ///     Indicates that the deployment request has been queued and is awaiting further processing.
+    ///     This status reflects that the deployment has not yet started and is in a pending state.
     /// </summary>
-    Pending,
+    Queued,
 
     /// <summary>
-    ///     The deployment is currently in progress.
+    ///     Represents the status of a deployment that is currently in the building stage,
+    ///     where the application or project is being compiled or prepared for deployment.
     /// </summary>
-    InProgress,
+    Building,
 
     /// <summary>
-    ///     The deployment has completed successfully.
+    ///     Represents the deployment status where the process is in the initial stage of being started.
+    ///     This state indicates that the deployment has been initiated but is not yet actively running.
     /// </summary>
-    Succeeded,
+    Starting,
 
     /// <summary>
-    ///     The deployment failed to complete.
+    ///     Represents the state of a deployment that is currently running.
+    ///     Indicates that the deployment process has been successfully initiated
+    ///     and is actively operating or executing as intended.
+    /// </summary>
+    Running,
+
+    /// <summary>
+    ///     Represents the state of a deployment that has been manually
+    ///     or programmatically stopped after being previously started.
+    /// </summary>
+    Stopped,
+
+    /// <summary>
+    ///     Represents a deployment status where the deployment process has failed.
+    ///     This status indicates that an error or issue occurred during the deployment
+    ///     lifecycle, and the deployment did not complete successfully.
     /// </summary>
     Failed
 }
