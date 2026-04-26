1ed343ebee9221e48939235d3706026682f80e78
Toronyi Zsombor <toronyi.zsombor.dev@gmail.com>
2026-04-26 18:26:51 +0200

    Refactor XML Documentation for Consistency and Clarity

--- Core/Entities/BaseEntity.cs
+++ Core/Entities/BaseEntity.cs
@@ -1,7 +1,7 @@
 namespace Core.Entities;
 
 /// <summary>
-///    Represents the base entity class that all other entities in the application will inherit from.
+///     Represents the base entity class that all other entities in the application will inherit from.
 /// </summary>
 public abstract class BaseEntity
 {
